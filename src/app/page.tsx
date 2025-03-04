2e79a2266ab02499d9bfc105bfae856ec9b44d43
AnamariaMarjanovic <marjanovic.anamaria@outlook.com>
2025-03-04 06:17:34 +0100

    feat: add @vercel/analytics dependency and integrate analytics component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { ExperiencesSection } from '@/sections/Experiences';
 import { AboutSection } from '@/sections/About';
 import { ContactSection } from '@/sections/Contact';
 import { Footer } from '@/sections/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Main() {
   return (
     <div className='relative z-[-1]'>
+      <Analytics />
       <Header />
       <IntroSection id="intro"/>
       <ExperiencesSection id="experiences" />
